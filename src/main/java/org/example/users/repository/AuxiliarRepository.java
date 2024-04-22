f7cef0a2d7aa0e951c697203028dcca5a8d0938b
Mario Reyes <mario-09@live.com.ar>
2024-04-22 09:31:21 -0600

    modifying policy table

--- src/main/java/org/example/users/repository/AuxiliarRepository.java
+++ src/main/java/org/example/users/repository/AuxiliarRepository.java
@@ -12,38 +12,38 @@ import java.util.List;
 @Repository
 public interface AuxiliarRepository extends JpaRepository<Auxiliar, String> {
 
-    @Query(value = "SELECT * from policyObjFile WHERE account_id=:account_id AND fecha BETWEEN :initial_date AND :final_date", nativeQuery = true)
+    @Query(value = "SELECT * from policy WHERE account_id=:account_id AND fecha BETWEEN :initial_date AND :final_date", nativeQuery = true)
     List<Auxiliar> getAuxiliar(String account_id, String initial_date, String final_date);
 
-    @Query(value = "SELECT * from policyObjFile WHERE account_id=:account_id ", nativeQuery = true)
+    @Query(value = "SELECT * from policy WHERE account_id=:account_id ", nativeQuery = true)
     List<Auxiliar> getAuxiliarById(String account_id);
 
-    @Query(value = "SELECT * FROM dbmaster.policyObjFile pof WHERE pof.cuenta=:cuenta", nativeQuery = true)
+    @Query(value = "SELECT * FROM dbmaster.policy pof WHERE pof.cuenta=:cuenta", nativeQuery = true)
     List<Auxiliar> valuesOfTable(String cuenta);
 
-    @Query(value = "SELECT SUM(pof.debe) FROM  dbmaster.policyObjFile pof where pof.cuenta=:cuenta", nativeQuery = true)
+    @Query(value = "SELECT SUM(pof.debe) FROM  dbmaster.policy pof where pof.cuenta=:cuenta", nativeQuery = true)
     String getSumInicial(String cuenta);
 
-    @Query(value = "SELECT * FROM dbmaster.policyObjFile pof WHERE pof.cuenta=:cuenta AND fecha BETWEEN :initial_date AND :final_date", nativeQuery = true)
+    @Query(value = "SELECT * FROM dbmaster.policy pof WHERE pof.cuenta=:cuenta AND fecha BETWEEN :initial_date AND :final_date", nativeQuery = true)
     List<Auxiliar> valuesOfPDF(String cuenta, String initial_date, String final_date);
 
-    @Query(value = "SELECT SUM(pof.debe) FROM  dbmaster.policyObjFile pof where pof.cuenta=:cuenta", nativeQuery = true)
+    @Query(value = "SELECT SUM(pof.debe) FROM  dbmaster.policy pof where pof.cuenta=:cuenta", nativeQuery = true)
     String getSumCargo(String cuenta);
 
-    @Query(value = "SELECT SUM(pof.haber) FROM  dbmaster.policyObjFile pof where pof.cuenta=:cuenta", nativeQuery = true)
+    @Query(value = "SELECT SUM(pof.haber) FROM  dbmaster.policy pof where pof.cuenta=:cuenta", nativeQuery = true)
     String getSumAbono(String cuenta);
 
     /**/
-    @Query(value = "SELECT SUM(pof.debe) FROM  dbmaster.policyObjFile pof where pof.cuenta=:account_id", nativeQuery = true)
+    @Query(value = "SELECT SUM(pof.debe) FROM  dbmaster.policy pof where pof.cuenta=:account_id", nativeQuery = true)
     String getSumDebeByAccountId(String account_id);
 
-    @Query(value = "SELECT SUM(pof.haber) FROM  dbmaster.policyObjFile pof where pof.cuenta=:account_id", nativeQuery = true)
+    @Query(value = "SELECT SUM(pof.haber) FROM  dbmaster.policy pof where pof.cuenta=:account_id", nativeQuery = true)
     String getSumHaberByAccountId(String account_id);
 
     /**/
 
 
-    @Query(value = "SELECT DISTINCT pof.fecha FROM  dbmaster.policyObjFile pof, dbmaster.user u  where u.token=:token", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT pof.fecha FROM  dbmaster.policy pof, dbmaster.user u  where u.token=:token", nativeQuery = true)
     List<String> getDatesByUser(String token);
 
 }
