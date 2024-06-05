e0bbda578a0c6cb0156f62ba56c5666986b3777e
Mario Reyes <mario-09@live.com.ar>
2024-06-05 09:50:38 -0600

    Revert "adding logic to get values from DB, if not create policy"
    This reverts commit a78cd383e19d4cd4d257b2226bfee583f5e31ffd.

--- src/main/java/org/example/users/repository/SaveObjRepository.java
+++ src/main/java/org/example/users/repository/SaveObjRepository.java
@@ -1,15 +1,10 @@
 package org.example.users.repository;
 
+
 import org.example.users.model.PolicytoDB;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 
 @Repository
 public interface SaveObjRepository extends JpaRepository<PolicytoDB, Long> {
-
-    @Query(value = "SELECT DISTINCT (usuario)  FROM dbmaster.policy  WHERE account_id=:account_id", nativeQuery = true)
-   String getValuesOfDB(String account_id);
-
 }
